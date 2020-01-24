1261f7f063112ed566d82f917063ab3889817979
Nour EL HASSANE <hn_el_hassane@esi.dz>
2020-01-24 17:28:19 +0100

    updates

--- App.js
+++ App.js
@@ -1,54 +1,130 @@
-import { AppLoading } from "expo";
-import { Asset } from "expo-asset";
-import * as Font from "expo-font";
-import React, { useState } from "react";
-import { Platform, StatusBar, StyleSheet, View } from "react-native";
+import React, { Component } from 'react';
+import { Text, View, Linking, TouchableHighlight, PermissionsAndroid, Platform, StyleSheet} from 'react-native';
+import { CameraKitCameraScreen, } from 'react-native-camera-kit';
 
-import AppNavigator from "./navigation/AppNavigator";
-
-export default function App(props) {
-  const [isLoadingComplete, setLoadingComplete] = useState(false);
-  if (!isLoadingComplete && !props.skipLoadingScreen) {
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      //variable to hold the qr value
+      qrvalue: '',
+      opneScanner: false,
+    };
+  }
+  onOpenlink() {
+    //Function to open URL, If scanned 
+    Linking.openURL(this.state.qrvalue);
+    //Linking used to open the URL in any browser 
+  }
+  onBarcodeScan(qrvalue) {
+    //called after te successful scanning of QRCode/Barcode
+    this.setState({ qrvalue: qrvalue });
+    this.setState({ opneScanner: false });
+  }
+  onOpneScanner() {
+    var that =this;
+    //To Start Scanning
+    if(Platform.OS === 'android'){
+      async function requestCameraPermission() {
+        try {
+          const granted = await PermissionsAndroid.request(
+            PermissionsAndroid.PERMISSIONS.CAMERA,{
+              'title': 'CameraExample App Camera Permission',
+              'message': 'CameraExample App needs access to your camera '
+            }
+          )
+          if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+            //If CAMERA Permission is granted
+            that.setState({ qrvalue: '' });
+            that.setState({ opneScanner: true });
+          } else {
+            alert("CAMERA permission denied");
+          }
+        } catch (err) {
+          alert("Camera permission err",err);
+          console.warn(err);
+        }
+      }
+      //Calling the camera permission function
+      requestCameraPermission();
+    }else{
+      that.setState({ qrvalue: '' });
+      that.setState({ opneScanner: true });
+    }    
+  }
+  render() {
+    let displayModal;
+    //If qrvalue is set then return this view
+    if (!this.state.opneScanner) {
+      return (
+        <View style={styles.container}>
+            <Text style={styles.heading}>React Native QR Code Example</Text>
+            <Text style={styles.simpleText}>{this.state.qrvalue ? 'Scanned QR Code: '+this.state.qrvalue : ''}</Text>
+            {this.state.qrvalue.includes("http") ? 
+              <TouchableHighlight
+                onPress={() => this.onOpenlink()}
+                style={styles.button}>
+                  <Text style={{ color: '#FFFFFF', fontSize: 12 }}>Open Link</Text>
+              </TouchableHighlight>
+              : null
+            }
+            <TouchableHighlight
+              onPress={() => this.onOpneScanner()}
+              style={styles.button}>
+                <Text style={{ color: '#FFFFFF', fontSize: 12 }}>
+                Open QR Scanner
+                </Text>
+            </TouchableHighlight>
+        </View>
+      );
+    }
     return (
-      <AppLoading
-        startAsync={loadResourcesAsync}
-        onError={handleLoadingError}
-        onFinish={() => handleFinishLoading(setLoadingComplete)}
-      />
-    );
-  } else {
-    return (
-      <View style={styles.container}>
-        {Platform.OS === "ios" && <StatusBar barStyle="default" />}
-        <AppNavigator />
+      <View style={{ flex: 1 }}>
+        <CameraKitCameraScreen
+          showFrame={false}
+          //Show/hide scan frame
+          scanBarcode={true}
+          //Can restrict for the QR Code only
+          laserColor={'blue'}
+          //Color can be of your choice
+          frameColor={'yellow'}
+          //If frame is visible then frame color
+          colorForScannerFrame={'black'}
+          //Scanner Frame color
+          onReadCode={event =>
+            this.onBarcodeScan(event.nativeEvent.codeStringValue)
+          }
+        />
       </View>
     );
   }
 }
-
-async function loadResourcesAsync() {
-  await Promise.all([
-    Font.loadAsync({
-      // We include SpaceMono because we use it in HomeScreen.js. Feel free to
-      // remove this if you are not using it in your app
-      "Ubuntu-Medium": require("./assets/fonts/Ubuntu-Medium.ttf")
-    })
-  ]);
-}
-
-function handleLoadingError(error) {
-  // In this case, you might want to report the error to your error reporting
-  // service, for example Sentry
-  console.warn(error);
-}
-
-function handleFinishLoading(setLoadingComplete) {
-  setLoadingComplete(true);
-}
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff"
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor:'white'
+  },
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#2c3539',
+    padding: 10,
+    width:300,
+    marginTop:16
+  },
+  heading: { 
+    color: 'black', 
+    fontSize: 24, 
+    alignSelf: 'center', 
+    padding: 10, 
+    marginTop: 30 
+  },
+  simpleText: { 
+    color: 'black', 
+    fontSize: 20, 
+    alignSelf: 'center', 
+    padding: 10, 
+    marginTop: 16
   }
 });
